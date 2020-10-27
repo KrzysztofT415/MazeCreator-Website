a96c45ea6605e706ef43a7d67fd5646758bf9be4
KrzysztofT415 <Krzysztof15042000@wp.pl>
2020-10-27 09:18:31 +0100

    Deleted global variables due to not working on FF.

--- src/Board.js
+++ src/Board.js
@@ -1,39 +1,41 @@
 class Board {
-    #hexes = []
-    #size = 50
+    hexes = []
+    size = 50
     constructor() {
+        this.hexes = []
+        this.size = 50
         let width = 10
         let height = 10
 
         let sth = [...Array(width).keys()].map(
             x => x = [...Array(height).keys()].map(
-                y => this.#hexes = [...this.#hexes, new Hex(x, y - Math.floor(x / 2))]
+                y => this.hexes = [...this.hexes, new Hex(x, y - Math.floor(x / 2))]
             )
         )
 
         document.getElementById('reset').addEventListener('click', () => {
-            this.#hexes = []
+            this.hexes = []
         })
     }
 
     addHexOnPoint = (x, y) => {
         let hex = this.pixel_to_flat_hex(x, y)
         let object = document.getElementById(`${hex.q}-${hex.r}`)
-        if (object === null) { this.#hexes = [...this.#hexes, new Hex(hex.q, hex.r)] }
+        if (object === null) { this.hexes = [...this.hexes, new Hex(hex.q, hex.r)] }
     }
 
     removeHexOnPoint = (x, y) => {
         let hex = this.pixel_to_flat_hex(x, y)
         let object = document.getElementById(`${hex.q}-${hex.r}`)
         if (object !== null) {
-            this.#hexes = this.#hexes.filter(v => ((v.coordinates.q !== hex.q) && (v.coordinates.r !== hex.r)))
+            this.hexes = this.hexes.filter(v => ((v.coordinates.q !== hex.q) && (v.coordinates.r !== hex.r)))
             document.getElementById('grid').removeChild(object)
         }
     }
 
     pixel_to_flat_hex = (x, y) => {
-        let q = (2. / 3 * x) / this.#size
-        let r = (-1. / 3 * x + Math.sqrt(3) / 3 * y) / this.#size
+        let q = (2. / 3 * x) / this.size
+        let r = (-1. / 3 * x + Math.sqrt(3) / 3 * y) / this.size
         return this.hex_round({q: q, r: r})
     }
 
@@ -73,5 +75,5 @@ class Board {
         return this.cube_to_axial(this.cube_round(this.axial_to_cube(hex)))
     }
 
-    get size() { return this.#size }
+    get size() { return this.size }
 }
--- src/Hex.js
+++ src/Hex.js
@@ -1,35 +1,35 @@
 class Hex {
-    #coordinates
-    #object
-    #center
-    #corners
-    #edges
+    coordinates
+    object
+    center
+    corners
+    edges
     static directions = [[1, 0],[0, 1],[-1, 1],[-1, 0],[0, -1],[1, -1]]
 
     constructor(q, r) {
-        this.#coordinates = {q: q, r: r}
+        this.coordinates = {q: q, r: r}
 
         let size = 50
-        this.#center = [q * size * 1.5, (r + q / 2.0) * size * Math.sqrt(3)]
+        this.center = [q * size * 1.5, (r + q / 2.0) * size * Math.sqrt(3)]
 
-        this.#corners = [...Array(6).keys()].map(
-            v => [this.#center[0] + size * Math.cos(Math.PI  * v / 3), this.#center[1] + size * Math.sin(Math.PI  * v / 3)])
+        this.corners = [...Array(6).keys()].map(
+            v => [this.center[0] + size * Math.cos(Math.PI  * v / 3), this.center[1] + size * Math.sin(Math.PI  * v / 3)])
 
-        this.#edges = [...Array(6).keys()].map(
-            v => [this.#corners[v], this.#corners[(v + 1) % 6]])
+        this.edges = [...Array(6).keys()].map(
+            v => [this.corners[v], this.corners[(v + 1) % 6]])
 
-        this.#object = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
-        this.#object.setAttribute('points', this.#corners.reduce((acc, v) => acc + ' ' + v.toString()))
-        this.#object.id = q + '-' + r
-        this.#object.classList.add('type-hex', 'q-' + q, 'r-' + r, 'q-' + (q % 2 !== 0 ? 'odd' : 'even'), 'r-' + (r % 2 !== 0 ? 'odd' : 'even'), 'status-empty')
+        this.object = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
+        this.object.setAttribute('points', this.corners.reduce((acc, v) => acc + ' ' + v.toString()))
+        this.object.id = q + '-' + r
+        this.object.classList.add('type-hex', 'q-' + q, 'r-' + r, 'q-' + (q % 2 !== 0 ? 'odd' : 'even'), 'r-' + (r % 2 !== 0 ? 'odd' : 'even'), 'status-empty')
 
         let values = getCurrentRootProperties()
-        this.#object.style.transform = `translate(${values.tx}px, ${values.ty}px) scale(${values.sc})`
+        this.object.style.transform = `translate(${values.tx}px, ${values.ty}px) scale(${values.sc})`
 
-        document.getElementById('grid').appendChild(this.#object)
+        document.getElementById('grid').appendChild(this.object)
     }
 
-    set setState(newState) { this.#object.classList.replace(``,`status-${newState}`) }
+    set setState(newState) { this.object.classList.replace(``,`status-${newState}`) }
 
-    get coordinates() { return this.#coordinates }
+    get coordinates() { return this.coordinates }
 }
